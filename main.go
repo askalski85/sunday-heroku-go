c393d0c3c51b88fe13b385f6554215b9e4f40192
Arkadiusz Skalski <arkadiusz.skalski@tomtom.com>
2019-01-31 15:46:37 +0100

    Dynamic port number

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+
 	router := gin.Default()
   router.GET("/foo", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "bar",
 		})
 	})
-	router.Run(":8085")
+	router.Run(":" + port)
 }
